92ff6915a838ad6348af2b4c9790813572789be0
Palina Manenok <29563311+PManenok@users.noreply.github.com>
2018-09-20 23:06:28 +0300

    Homework 2 done

--- app/src/main/java/by/itacademy/palina/homework/MainActivity.java
+++ app/src/main/java/by/itacademy/palina/homework/MainActivity.java
@@ -1,13 +1,38 @@
 package by.itacademy.palina.homework;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import by.itacademy.palina.homework.hw1.HW1;
+import by.itacademy.palina.homework.hw2.HW2;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnHW1;
+    private Button btnHW2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnHW1 = findViewById(R.id.btnHW1);
+        btnHW1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HW1.class);
+                startActivity(intent);
+            }
+        });
+        btnHW2 = findViewById(R.id.btnHW2);
+        btnHW2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HW2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/by/itacademy/palina/homework/hw2/HW2.java
@@ -0,0 +1,15 @@
+package by.itacademy.palina.homework.hw2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import by.itacademy.palina.homework.R;
+
+public class HW2 extends Activity {
+// Австрия, Польша, Италия, Колумбия, Мадагаскар, Таиланд и Дании
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hw2);
+    }
+}
